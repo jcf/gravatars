36c2311b3c19356b656deceb17bbe8675f11cdca
James Conroy-Finn <james@logi.cl>
2012-10-01 20:38:11 +0100

    Configure RSpec generators

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,11 @@ end
 
 module Avatar
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.test_framework :rspec, routing_specs: false, views: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
