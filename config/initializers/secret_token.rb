892c3fc0d5a7f717e7be79240d0f92842ea1836d
James Conroy-Finn <james@logi.cl>
2012-10-01 20:38:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avatar::Application.config.secret_token = 'cc65229b64896de62c15a9669408a5b1b6a3151f8774691ee851bbc551490ec3ac4390f3c9a5900205ace54a795336156fc6df2ec618c1ffb55767212eae15b9'
