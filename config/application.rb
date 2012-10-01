9306858babfdc4d699fb956a147d059599104601
James Conroy-Finn <james@logi.cl>
2012-10-01 23:30:36 +0100

    Heroku configuration

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,21 @@
 require File.expand_path('../boot', __FILE__)
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_resource/railtie'
+require 'sprockets/railtie'
+
 if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
 end
+
 module Avatar
   class Application < Rails::Application
     config.generators do |g|
       g.test_framework :rspec, routing_specs: false, views: false
     end
-    config.encoding = "utf-8"
+
+    config.encoding = 'utf-8'
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
     config.active_record.whitelist_attributes = true
@@ -20,5 +23,7 @@ module Avatar
     config.assets.version = '1.0'
 
     config.ember.variant = :development
+
+    config.assets.initialize_on_precompile = false
   end
 end
